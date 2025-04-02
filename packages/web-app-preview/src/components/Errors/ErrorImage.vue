75070789e5bd849c14f4b74d37d166041080c338
Benedikt Kulmann <b.kulmann@opencloud.eu>
2025-04-02 17:57:57 +0200

    feat(WIP): preview image loading failover

---
+++ packages/web-app-preview/src/components/Errors/ErrorImage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="oc-width-1-1 oc-flex oc-flex-column oc-flex-middle oc-flex-center">
+    <oc-icon name="file-damage" size="xlarge" color="var(--oc-role-error)" />
+    <p>
+      {{ $gettext('Failed to load "%{filename}"', { filename: activeMediaFileCached.name }) }}
+    </p>
+    <oc-button @click="loadOriginalFile">
+      {{ $gettext('Load Original') }}
+    </oc-button>
+  </div>
+</template>
+<script setup lang="ts">
+import { CachedFile } from '../../helpers/types'
+
+const { activeMediaFileCached } = defineProps<{
+  activeMediaFileCached: CachedFile
+}>()
+
+const emit = defineEmits<{
+  (e: 'replaceUrl', { url }: { url: string }): void
+}>()
+
+const loadOriginalFile = () => {
+  emit('replaceUrl', { url: activeMediaFileCached.fallbackUrl })
+}
+</script>
