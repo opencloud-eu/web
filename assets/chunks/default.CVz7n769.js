ec35252a79785d30cd583e755a78eea171a443da
JammingBen <50302941+JammingBen@users.noreply.github.com>
2025-07-08 06:12:22 +0000

    Merge pull request #912 from opencloud-eu/revert-color-hacks
    feat: revert material design color hacks

--- assets/chunks/default.CVz7n769.js
+++
@@ -1,1 +0,0 @@
-import{r as s,M as a,z as c,Y as p,w as r}from"./framework.BUi0CrwJ.js";import"./theme._hH23DCf.js";const f=s({__name:"default",setup(l){const e=a([10,20,50][0]),t=o=>{e.value=o};return(o,i)=>{const n=p("oc-page-size");return r(),c(n,{label:"Items per page",options:[10,20,50],selected:e.value,onChange:t},null,8,["selected"])}}});export{f as default};
