6cc283b460718a4aa1d5a29880004b04a6df45ab
Jannik Stehle <j.stehle@opencloud.eu>
2025-08-13 12:28:22 +0200

    feat: set up tailwind

---
+++ packages/web-runtime/src/tailwind.ts
@@ -0,0 +1,6 @@
+/**
+ * This file acts as one of the main entry points for vite. It ensures that
+ * Tailwind CSS always comes before any other CSS, which is necessary for the
+ * layer styles to be applied correctly.
+ */
+import './tailwind.css'
