2f747ba2c3b88bdf306400523b550719ab65f9b9
Jannik Stehle <j.stehle@opencloud.eu>
2025-08-27 15:27:53 +0200

    fix: right sidebar position

--- packages/web-app-files/src/components/FilesViewWrapper.vue
+++ packages/web-app-files/src/components/FilesViewWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="files-view-wrapper grid grid-cols-1 focus:outline-0">
+  <div class="files-view-wrapper grid grid-cols-1 flex-1 focus:outline-0">
     <div id="files-view" v-bind="$attrs" class="outline-0">
       <slot />
     </div>
