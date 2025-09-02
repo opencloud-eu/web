6660c8858d28ba3472a799c0eca268dbc394eb68
Jannik Stehle <j.stehle@opencloud.eu>
2025-09-02 10:24:48 +0200

    test: reintroduce position class test for OcNotifications

--- packages/design-system/src/components/OcNotifications/OcNotifications.spec.ts
+++ packages/design-system/src/components/OcNotifications/OcNotifications.spec.ts
@@ -5,6 +5,20 @@ describe('OcNotifications', () => {
   function getWrapper(options = {}) {
     return shallowMount(OcNotifications, options)
   }
+  describe('position prop', () => {
+    it.each([
+      { position: 'top-left', expectedClasses: ['top-2', 'left-2'] },
+      { position: 'top-center', expectedClasses: ['top-2', 'inset-x-0'] },
+      { position: 'top-right', expectedClasses: ['top-2', 'right-2'] }
+    ])('should set provided position as class for wrapper', ({ position, expectedClasses }) => {
+      const wrapper = getWrapper({
+        props: { position }
+      })
+      expectedClasses.forEach((expectedClass) => {
+        expect(wrapper.attributes('class')).toContain(expectedClass)
+      })
+    })
+  })
 
   it('should render provided slot html', () => {
     const wrapper = getWrapper({
