b535a5fad8097dd8512b0150aa87e41dfd9735aa
Viktor Scharf <scharf.vi@gmail.com>
2025-09-03 16:48:59 +0200

    fix: remove flaky resourceExists

--- tests/e2e/support/objects/app-files/resource/utils.ts
+++ tests/e2e/support/objects/app-files/resource/utils.ts
@@ -1,41 +1,9 @@
-import { errors, Page, Locator } from '@playwright/test'
+import { Page, Locator } from '@playwright/test'
 import util from 'util'
 
 const resourceNameSelector = '#files-space-table [data-test-resource-name="%s"]'
 const showLinkShareButton =
   '//span[@data-test-resource-name="%s"]/ancestor::tr[contains(@class, "oc-tbody-tr")]//button[contains(@data-test-indicator-type, "%s")]'
-
-/**
- * one of the few places where timeout should be used, as we also use this to detect the absence of an element
- * it is not possible to differentiate between `element not there yet` and `element not loaded yet`.
- *
- * @param page
- * @param name
- * @param timeout
- */
-export const resourceExists = async ({
-  page,
-  name,
-  timeout = 500
-}: {
-  page: Page
-  name: string
-  timeout?: number
-}): Promise<boolean> => {
-  let exist = true
-  await page
-    .locator(util.format(resourceNameSelector, name))
-    .waitFor({ timeout })
-    .catch((e) => {
-      if (!(e instanceof errors.TimeoutError)) {
-        throw e
-      }
-
-      exist = false
-    })
-
-  return exist
-}
 
 export const waitForResources = async ({
   page,
