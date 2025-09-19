e3fc3e9ec4048dd778317897eec3a975b43404bd
openclouders <devops@opencloud.eu>
2025-09-19 00:12:20 +0000

    [tx] updated from transifex

--- assets/chunks/default.BBmZKeEy.js
+++
@@ -1,1 +0,0 @@
-import{r as s,M as a,z as c,Y as p,w as r}from"./framework.jfgvqFV1.js";import"./theme.CdKv62Gu.js";const f=s({__name:"default",setup(l){const e=a([10,20,50][0]),t=o=>{e.value=o};return(o,i)=>{const n=p("oc-page-size");return r(),c(n,{label:"Items per page",options:[10,20,50],selected:e.value,onChange:t},null,8,["selected"])}}});export{f as default};
