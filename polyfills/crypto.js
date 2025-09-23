f1da5b0c011dbf240fb58d79ea2e21c14e0331f9
Jannik Stehle <j.stehle@opencloud.eu>
2025-09-23 08:09:56 +0200

    fix: @noble/hashes import paths after v2.0.1 bump

--- polyfills/crypto.js
+++ polyfills/crypto.js
@@ -1,6 +1,6 @@
-import { pbkdf2 } from '@noble/hashes/pbkdf2'
+import { pbkdf2 } from '@noble/hashes/pbkdf2.js'
 import { sha512 } from '@noble/hashes/sha2.js'
-import { randomBytes } from '@noble/hashes/utils'
+import { randomBytes } from '@noble/hashes/utils.js'
 
 export const pbkdf2Sync = (password, salt, c, dkLen) => {
   return Buffer.from(pbkdf2(sha512, password, salt, { c, dkLen }))
