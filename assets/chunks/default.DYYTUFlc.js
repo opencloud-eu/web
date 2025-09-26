1d6c1fbe0bedc6d8092686d9243ad95ad0a9404a
openclouders <devops@opencloud.eu>
2025-09-26 00:09:55 +0000

    [tx] updated from transifex

---
+++ assets/chunks/default.DYYTUFlc.js
@@ -0,0 +1,1 @@
+import{r as a,M as s,O as p,z as r,Y as u,w as c}from"./framework.D8L2KA0h.js";import"./theme.BtjrB0gS.js";const _=a({__name:"default",setup(m){const e=s();return p(e,o=>{console.log(o)}),(o,l)=>{const t=u("oc-file-input");return c(),r(t,{modelValue:e.value,"onUpdate:modelValue":l[0]||(l[0]=n=>e.value=n),label:"Select file"},null,8,["modelValue"])}}});export{_ as default};
