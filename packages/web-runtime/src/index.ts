791241d09b3ffcd2f01979cf5fc3a4301eddf0eb
Alexander Ackermann <abackermann91@gmail.com>
2025-10-15 20:35:40 +0200

    load groupware config from server

--- packages/web-runtime/src/index.ts
+++ packages/web-runtime/src/index.ts
@@ -218,9 +218,10 @@ export const bootstrapApp = async (configurationPath: string, appsReadyCallback:
       })
 
       await announceGroupware({
-        groupwareConfigStore,
         clientService,
-        capabilityStore
+        configStore,
+        capabilityStore,
+        groupwareConfigStore
       })
 
       // Register SSE event listeners
