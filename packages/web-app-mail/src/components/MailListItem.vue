9fb2e93a477eb6a3105a9c801a35466a393208f8
tammi-23 <t.thamm@opencloud.eu>
2025-10-16 11:28:14 +0200

    wip: Inbox.vue

---
+++ packages/web-app-mail/src/components/MailListItem.vue
@@ -0,0 +1,1 @@
+<template>Mail List Item</template>
--- packages/web-app-mail/src/views/Inbox.vue
+++ packages/web-app-mail/src/views/Inbox.vue
@@ -1,1 +1,30 @@
 <template>MOIN MOIN</template>
+
+<script lang="ts">
+import { urlJoin } from '@opencloud-eu/web-client'
+import { useClientService, useConfigStore } from '@opencloud-eu/web-pkg'
+import { defineComponent, onMounted } from 'vue'
+import { useTask } from 'vue-concurrency'
+
+const configStore = useConfigStore()
+const clientService = useClientService()
+
+const loadAllMailsTask = useTask(function* (signal) {
+  try {
+    const mails = yield clientService.httpAuthenticated.get(
+      urlJoin(configStore.groupwareUrl, 'accounts/all/emails/latest/summary'),
+      {
+        signal
+      }
+    )
+    console.log(mails)
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+onMounted(() => {
+  loadAllMailsTask.perform()
+  console.log('Inbox mounted')
+})
+</script>
