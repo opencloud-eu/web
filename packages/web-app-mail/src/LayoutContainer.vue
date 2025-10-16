fd888f23016146755bdacf93c02e36ce0cc8aa63
tammi-23 <t.thamm@opencloud.eu>
2025-10-16 10:31:25 +0200

    wip: webappmail

---
+++ packages/web-app-mail/src/LayoutContainer.vue
@@ -0,0 +1,7 @@
+<template>
+  <main id="mail" class="p-4 overflow-auto">
+    <router-view data-testid="mail-router-view" />
+  </main>
+</template>
+
+<script lang="ts"></script>
---
+++ packages/web-app-mail/src/index.ts
@@ -0,0 +1,84 @@
+import translations from '../l10n/translations.json'
+import { useGettext } from 'vue3-gettext'
+import { computed, unref } from 'vue'
+import {
+  AppMenuItemExtension,
+  defineWebApplication,
+  Extension,
+  useAbility,
+  useUserStore
+} from '@opencloud-eu/web-pkg'
+import { urlJoin } from '@opencloud-eu/web-client'
+import { RouteRecordRaw } from 'vue-router'
+import { APPID } from './appid'
+
+export default defineWebApplication({
+  setup({ applicationConfig }) {
+    const { $gettext } = useGettext()
+
+    const appInfo = {
+      name: $gettext('Mail'),
+      id: APPID,
+      icon: 'mail',
+      color: '#0478d4'
+    }
+
+    const routes: RouteRecordRaw[] = [
+      {
+        path: '/',
+        name: 'root',
+        component: () => import('./LayoutContainer.vue'),
+        redirect: urlJoin(appInfo.id, 'list'),
+
+        meta: {
+          authContext: 'user'
+        },
+        children: [
+          {
+            path: 'all',
+            name: 'all',
+            meta: {
+              authContext: 'user'
+            },
+            children: [
+              {
+                path: 'inbox',
+                name: 'inbox',
+                component: () => import('./views/Inbox.vue'),
+                meta: {
+                  authContext: 'user',
+                  title: $gettext('All emails')
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+
+    const menuItemExtension: AppMenuItemExtension = {
+      id: `app.${appInfo.id}.menuItem`,
+      type: 'appMenuItem',
+      label: () => appInfo.name,
+      color: appInfo.color,
+      icon: appInfo.icon,
+      priority: 30,
+      path: urlJoin(appInfo.id)
+    }
+    const extensions = computed(() => {
+      const result: Extension[] = []
+
+      // Appaktiv wenn if groupware=true
+      result.push(menuItemExtension)
+
+      return result
+    })
+
+    return {
+      appInfo,
+      routes,
+      translations,
+      extensions
+    }
+  }
+})
