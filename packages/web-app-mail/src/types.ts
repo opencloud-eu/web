c63fa6bcb291252e3b2e1c932152a307bacd693d
tammi-23 <t.thamm@opencloud.eu>
2025-10-16 17:44:42 +0200

    wip: add MailListItem

---
+++ packages/web-app-mail/src/types.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod'
+
+export const MailAddressSchema = z.object({
+  name: z.string().optional(),
+  email: z.string()
+})
+
+export const MailboxIdsSchema = z.record(z.string(), z.boolean())
+export const KeywordsSchema = z.record(z.string(), z.boolean())
+
+export const MailSchema = z.object({
+  id: z.string(),
+  accountId: z.string(),
+  threadId: z.string(),
+
+  subject: z.string(),
+  preview: z.string().optional(),
+
+  receivedAt: z.string().optional(),
+  sentAt: z.string().optional(),
+
+  size: z.number().optional(),
+
+  mailboxIds: MailboxIdsSchema,
+
+  from: z.array(MailAddressSchema).optional().default([]),
+  sender: z.array(MailAddressSchema).optional().default([]),
+  to: z.array(MailAddressSchema).optional().default([]),
+
+  keywords: KeywordsSchema.optional().default({})
+})
+
+export type Mail = z.infer<typeof MailSchema>
+export type MailAddress = z.infer<typeof MailAddressSchema>
+export type MailboxIds = z.infer<typeof MailboxIdsSchema>
+export type Keywords = z.infer<typeof KeywordsSchema>
--- packages/web-app-mail/src/views/Inbox.vue
+++ packages/web-app-mail/src/views/Inbox.vue
@@ -1,23 +1,47 @@
-<template>MOIN MOIN</template>
+<template>
+  <app-loading-spinner v-if="isLoading" />
+  <div v-else>
+    <oc-list>
+      <li class="px-2 py-4 border-b-2" v-for="mail in mails" :key="mail.id">
+        <MailListItem
+          :from="mail.from"
+          :preview="mail.preview"
+          :received-at="mail.receivedAt"
+          :sender="mail.sender"
+          :subject="mail.subject"
+          :is-unread="!mail.keywords?.['$seen']"
+        />
+      </li>
+    </oc-list>
+  </div>
+</template>
 
 <script setup lang="ts">
+import { z } from 'zod'
 import { urlJoin } from '@opencloud-eu/web-client'
-import { useClientService, useConfigStore } from '@opencloud-eu/web-pkg'
-import { onMounted } from 'vue'
+import { useClientService, useConfigStore, useGroupwareConfigStore } from '@opencloud-eu/web-pkg'
+import { ref, computed, onMounted } from 'vue'
 import { useTask } from 'vue-concurrency'
+import MailListItem from '../components/MailListItem.vue'
+import { Mail, MailSchema } from '../types'
 
 const configStore = useConfigStore()
 const clientService = useClientService()
 
+const mails = ref<Mail[]>([])
+const isLoading = computed(() => loadAllMailsTask.isRunning && !loadAllMailsTask.last)
+
 const loadAllMailsTask = useTask(function* (signal) {
   try {
-    const mails = yield clientService.httpAuthenticated.get(
-      urlJoin(configStore.groupwareUrl, 'accounts/all/emails/latest/summary'),
+    const { data } = yield clientService.httpAuthenticated.get(
+      // we need to change this to /all if it is available
+      urlJoin(configStore.groupwareUrl, 'accounts/all/emails/latest/summary?limit=50&seen=true'),
       {
         signal
       }
     )
-    console.log(mails)
+    console.log(data)
+    mails.value = z.array(MailSchema).parse(data)
   } catch (e) {
     console.error(e)
   }
