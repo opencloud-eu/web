0c31a6b40dbe2923136738ac953d29d281da429c
tammi-23 <t.thamm@opencloud.eu>
2025-10-17 15:41:29 +0200

    wip: maildetailview

--- packages/web-app-mail/src/views/Inbox.vue
+++ packages/web-app-mail/src/views/Inbox.vue
@@ -1,23 +1,37 @@
 <template>
   <app-loading-spinner v-if="isLoading" />
-  <div v-else>
-    <oc-list>
-      <no-content-message v-if="!mails.length" icon="mail-forbid">
+  <div class="flex" v-else>
+    <div class="w-1/4 border-r-2 pr-2">
+      <h1 v-text="$gettext('All emails')" />
+      <oc-list>
+        <no-content-message v-if="!mails.length" icon="mail-forbid" icon-fill-type="line">
+          <template #message>
+            <span v-text="$gettext('No mails in this inbox')" />
+          </template>
+        </no-content-message>
+        <li v-for="mail in mails" v-else :key="mail.id" class="px-2 py-4 border-b-2">
+          <MailListItem
+            :from="mail.from"
+            :preview="mail.preview"
+            :received-at="mail.receivedAt"
+            :sender="mail.sender"
+            :subject="mail.subject"
+            :is-unread="!mail.keywords?.['$seen']"
+            @click="loadMailDetails(mail)"
+          />
+        </li>
+      </oc-list>
+    </div>
+    <div class="w-3/4">
+      <no-content-message v-if="!selectedMail" icon="mail" icon-fill-type="line">
         <template #message>
-          <span v-text="$gettext('No mails in this inbox')" />
+          <span v-text="$gettext('No mail selected')" />
         </template>
       </no-content-message>
-      <li v-for="mail in mails" v-else :key="mail.id" class="px-2 py-4 border-b-2">
-        <MailListItem
-          :from="mail.from"
-          :preview="mail.preview"
-          :received-at="mail.receivedAt"
-          :sender="mail.sender"
-          :subject="mail.subject"
-          :is-unread="!mail.keywords?.['$seen']"
-        />
-      </li>
-    </oc-list>
+      <app-loading-spinner v-else-if="isMailDetailsLoading" />
+      <MailDetails v-else :mail="selectedMailDetails" />
+      {{ selectedMailDetails }}
+    </div>
   </div>
 </template>
 
@@ -25,9 +39,10 @@
 import { z } from 'zod'
 import { urlJoin } from '@opencloud-eu/web-client'
 import { NoContentMessage, useClientService, useConfigStore } from '@opencloud-eu/web-pkg'
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, unref } from 'vue'
 import { useTask } from 'vue-concurrency'
 import MailListItem from '../components/MailListItem.vue'
+import MailDetails from '../components/MailDetails.vue'
 import { Mail, MailSchema } from '../types'
 import { AppLoadingSpinner } from '@opencloud-eu/web-pkg/src'
 
@@ -35,8 +50,13 @@ const configStore = useConfigStore()
 const clientService = useClientService()
 
 const mails = ref<Mail[]>([])
+const selectedMail = ref<Mail>(null)
+const selectedMailDetails = ref<Mail>(null)
 const isLoading = computed(() => loadAllMailsTask.isRunning && !loadAllMailsTask.last)
-
+const isMailDetailsLoading = computed(
+  () => loadMailDetailsTask.isRunning && !loadMailDetailsTask.last
+)
+//loadAllEmailsTask
 const loadAllMailsTask = useTask(function* (signal) {
   try {
     const { data } = yield clientService.httpAuthenticated.get(
@@ -52,6 +72,31 @@ const loadAllMailsTask = useTask(function* (signal) {
     console.error(e)
   }
 })
+
+const loadMailDetailsTask = useTask(function* (signal) {
+  try {
+    console.log('loading mail details for', unref(selectedMail))
+    const { data } = yield clientService.httpAuthenticated.get(
+      urlJoin(
+        configStore.groupwareUrl,
+        `accounts/${unref(selectedMail).accountId}/emails/${unref(selectedMail).id}`
+      ),
+      {
+        signal
+      }
+    )
+    console.log(data)
+    selectedMailDetails.value = MailSchema.parse(data)
+    console.log('loaded mail details', unref(selectedMailDetails))
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+const loadMailDetails = (mail: Mail) => {
+  selectedMail.value = mail
+  loadMailDetailsTask.perform()
+}
 
 onMounted(() => {
   loadAllMailsTask.perform()
