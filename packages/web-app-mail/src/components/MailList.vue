36a04ccf6fb51ec5a6856dd5d36253e9f4db7e0c
Alexander Ackermann <abackermann91@gmail.com>
2025-10-24 13:06:45 +0200

    cleanup

---
+++ packages/web-app-mail/src/components/MailList.vue
@@ -0,0 +1,53 @@
+<template>
+  <h2 v-if="mailbox" class="text-lg ml-4" v-text="mailbox.name" />
+  <oc-list class="mail-list">
+    <no-content-message
+      v-if="!mails.length"
+      class="mail-list-empty"
+      icon="mail-forbid"
+      icon-fill-type="line"
+    >
+      <template #message>
+        <span v-text="$gettext('No mails in this mailbox')" />
+      </template>
+    </no-content-message>
+    <li
+      v-for="mail in mails"
+      v-else
+      :key="mail.id"
+      class="border-b-2"
+      :class="{ 'bg-role-secondary-container': selectedMail.id === mail.id }"
+    >
+      <oc-button
+        class="px-2 py-4 text-left w-full"
+        justify-content="left"
+        appearance="raw"
+        gap-size="none"
+        no-hover
+        @click="$emit('select-mail', mail.id)"
+      >
+        <MailListItem :mail="mail" />
+      </oc-button>
+    </li>
+  </oc-list>
+</template>
+
+<script setup lang="ts">
+import { NoContentMessage } from '@opencloud-eu/web-pkg'
+import MailListItem from './MailListItem.vue'
+import { Mail, Mailbox } from '../types'
+
+const {
+  mails,
+  mailbox = {},
+  selectedMail = {}
+} = defineProps<{
+  mails: Mail[]
+  mailbox?: Mailbox
+  selectedMail?: Mail
+}>()
+
+defineEmits<{
+  (e: 'select-mail', mailId: string): void
+}>()
+</script>
