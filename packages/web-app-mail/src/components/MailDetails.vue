fe83fde86bac59942bec0e432eaa6b07a2f04a37
Alexander Ackermann <abackermann91@gmail.com>
2025-10-25 14:39:22 +0200

    add mobile flow

--- packages/web-app-mail/src/components/MailDetails.vue
+++ packages/web-app-mail/src/components/MailDetails.vue
@@ -1,39 +1,46 @@
 <template>
   <app-loading-spinner v-if="isLoading" />
-  <div v-else-if="mail" class="mail-details">
-    <oc-button class="md:hidden mb-2" appearance="raw" no-hover @click="$emit('back')">
-      <oc-icon name="arrow-left" fill-type="line" />
-    </oc-button>
-    <div class="mail-details-subject font-bold flex justify-between items-center mt-1">
-      <h3 class="text-lg block truncate" v-text="mail.subject" />
-      <MailIndicators :mail="mail" />
-    </div>
-    <div class="mail-details-subheader mt-2 flex justify-between">
-      <oc-avatar :user-name="mail.from[0]?.name || mail.sender[0]?.name" />
-      <div class="mail-details-userinfo flex-1 ml-4">
-        <div class="font-bold text-xl truncate flex-1" v-text="fromName" />
-        <div class="truncate" v-text="fromEmail" />
+  <template v-else>
+    <no-content-message v-if="!mail" icon="mail" icon-fill-type="line">
+      <template #message>
+        <span v-text="$gettext('No mail selected')" />
+      </template>
+    </no-content-message>
+    <div v-else class="mail-details">
+      <oc-button class="md:hidden mb-2" appearance="raw" no-hover @click="$emit('back')">
+        <oc-icon name="arrow-left" fill-type="line" />
+      </oc-button>
+      <div class="mail-details-subject font-bold flex justify-between items-center mt-1">
+        <h3 class="text-lg block truncate" v-text="mail.subject" />
+        <MailIndicators :mail="mail" />
       </div>
-      <span class="mail-details-received-at" v-text="receivedAtRelativeDate" />
+      <div class="mail-details-subheader mt-2 flex justify-between">
+        <oc-avatar :user-name="mail.from[0]?.name || mail.sender[0]?.name" />
+        <div class="mail-details-userinfo flex-1 ml-4">
+          <div class="font-bold text-xl truncate flex-1" v-text="fromName" />
+          <div class="truncate" v-text="fromEmail" />
+        </div>
+        <span class="mail-details-received-at" v-text="receivedAtRelativeDate" />
+      </div>
+      <div class="mail-details-to mt-4">
+        <span class="mr-4" v-text="$gettext('To:')" />
+        <span class="truncate" v-text="sendToNames" />
+      </div>
+      <div class="mail-details-body mt-6" v-html="mailBody" />
+      <MailAttachmentList
+        v-if="mail.attachments.length"
+        class="mail-details-attachments my-6"
+        :attachments="mail.attachments"
+        :account-id="mail.accountId"
+      />
     </div>
-    <div class="mail-details-to mt-4">
-      <span class="mr-4" v-text="$gettext('To:')" />
-      <span class="truncate" v-text="sendToNames" />
-    </div>
-    <div class="mail-details-body mt-6" v-html="mailBody" />
-    <MailAttachmentList
-      v-if="mail.attachments.length"
-      class="mail-details-attachments my-6"
-      :attachments="mail.attachments"
-      :account-id="mail.accountId"
-    />
-  </div>
+  </template>
 </template>
 
 <script setup lang="ts">
 import type { Mail } from '../types'
 import { computed, unref } from 'vue'
-import { formatRelativeDateFromISO } from '@opencloud-eu/web-pkg/src'
+import { formatRelativeDateFromISO, NoContentMessage } from '@opencloud-eu/web-pkg/src'
 import { useGettext } from 'vue3-gettext'
 import { buildMailBody } from '../helpers'
 import MailAttachmentList from './MailAttachmentList.vue'
