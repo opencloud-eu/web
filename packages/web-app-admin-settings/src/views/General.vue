b6eee5fbfa4eb312f6742dacc9c936be0f7d457f
Jannik Stehle <j.stehle@opencloud.eu>
2025-10-27 10:18:09 +0100

    fix: add missing first breadcrumb on table size screens

--- packages/web-app-admin-settings/src/views/General.vue
+++ packages/web-app-admin-settings/src/views/General.vue
@@ -2,7 +2,11 @@
   <div>
     <app-template
       ref="template"
-      :breadcrumbs="breadcrumbs"
+      :breadcrumbs="[
+        {
+          text: $gettext('General')
+        }
+      ]"
       :show-app-bar="false"
       :is-side-bar-open="isSideBarOpen"
       :side-bar-active-panel="sideBarActivePanel"
@@ -17,49 +21,26 @@
   </div>
 </template>
 
-<script lang="ts">
-import { ComponentPublicInstance, defineComponent, useTemplateRef } from 'vue'
+<script setup lang="ts">
+import { ComponentPublicInstance, useTemplateRef } from 'vue'
 import AppTemplate from '../components/AppTemplate.vue'
 import InfoSection from '../components/General/InfoSection.vue'
 import DetailsPanel from '../components/General/SideBar/DetailsPanel.vue'
 import { useGettext } from 'vue3-gettext'
 import { useSideBar } from '@opencloud-eu/web-pkg'
 
-export default defineComponent({
-  components: {
-    AppTemplate,
-    InfoSection
-  },
-  setup() {
-    const template = useTemplateRef<ComponentPublicInstance<typeof AppTemplate>>('template')
-    const { $gettext } = useGettext()
+const template = useTemplateRef<ComponentPublicInstance<typeof AppTemplate>>('template')
+const { $gettext } = useGettext()
+const { isSideBarOpen, sideBarActivePanel } = useSideBar()
 
-    const sideBarAvailablePanels = [
-      {
-        name: 'DetailsPanel',
-        icon: 'settings-4',
-        title: () => $gettext('Details'),
-        component: DetailsPanel,
-        isRoot: () => true,
-        isVisible: () => true
-      }
-    ]
-
-    return {
-      template,
-      sideBarAvailablePanels,
-      ...useSideBar()
-    }
-  },
-  computed: {
-    breadcrumbs() {
-      return [
-        { text: this.$gettext('Administration Settings'), to: { path: '/admin-settings' } },
-        {
-          text: this.$gettext('General')
-        }
-      ]
-    }
+const sideBarAvailablePanels = [
+  {
+    name: 'DetailsPanel',
+    icon: 'settings-4',
+    title: () => $gettext('Details'),
+    component: DetailsPanel,
+    isRoot: () => true,
+    isVisible: () => true
   }
-})
+]
 </script>
