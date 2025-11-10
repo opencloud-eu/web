d7ead8ad6abef9b97998113bf248afb8749c2461
Jannik Stehle <j.stehle@opencloud.eu>
2025-11-10 15:45:12 +0100

    fix: initial default favicon
    Uses the OpenCloud logo as default favicon until the theme favicon is loaded. This fixes an issue where OpenCloud would not have any bookmark icon in Firefox due to the initially missing favicon.
    
    Unfortunately, we can't use `vueuse/head` because it would result in a duplicated `<link rel="">` tag.

--- packages/web-runtime/src/composables/head/useHead.ts
+++ packages/web-runtime/src/composables/head/useHead.ts
@@ -1,29 +1,19 @@
-import { computed, unref } from 'vue'
+import { computed } from 'vue'
 import { useHead as _useHead } from '@vueuse/head'
 import { getBackendVersion, getWebVersion } from '../../container/versions'
-import { useCapabilityStore, useThemeStore } from '@opencloud-eu/web-pkg'
-import { storeToRefs } from 'pinia'
+import { useCapabilityStore } from '@opencloud-eu/web-pkg'
 
 export const useHead = () => {
-  const themeStore = useThemeStore()
   const capabilityStore = useCapabilityStore()
-  const { currentTheme } = storeToRefs(themeStore)
 
-  const favicon = computed(() => unref(currentTheme).favicon)
-
-  _useHead(
-    computed(() => {
-      return {
-        meta: [
-          {
-            name: 'generator',
-            content: [getWebVersion(), getBackendVersion({ capabilityStore })]
-              .filter(Boolean)
-              .join(', ')
-          }
-        ],
-        ...(unref(favicon) && { link: [{ rel: 'icon', href: unref(favicon) }] })
+  _useHead({
+    meta: computed(() => [
+      {
+        name: 'generator',
+        content: [getWebVersion(), getBackendVersion({ capabilityStore })]
+          .filter(Boolean)
+          .join(', ')
       }
-    })
-  )
+    ])
+  })
 }
