270feb7f5ca3eba907728420056bbc8f774cbfbe
kulmann <b.kulmann@opencloud.eu>
2025-11-28 13:16:55 +0000

    Merge pull request #1673 from opencloud-eu/renovate/typescript-eslint-monorepo
    chore(deps): update typescript-eslint monorepo to v8.48.0

--- assets/chunks/default.DlrUnYbl.js
+++
@@ -1,1 +0,0 @@
-import{r as s,M as a,z as c,Y as p,w as r}from"./framework.TQY4xrJh.js";import"./theme.CF8jBTl9.js";const f=s({__name:"default",setup(l){const e=a([10,20,50][0]),t=o=>{e.value=o};return(o,i)=>{const n=p("oc-page-size");return r(),c(n,{label:"Items per page",options:[10,20,50],selected:e.value,onChange:t},null,8,["selected"])}}});export{f as default};
