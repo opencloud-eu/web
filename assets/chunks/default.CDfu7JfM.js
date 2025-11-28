570a0fc68d02ade1da6d8168a0e03922436cb1f7
kulmann <b.kulmann@opencloud.eu>
2025-11-28 12:23:36 +0000

    Merge pull request #1667 from opencloud-eu/renovate/zod-4.x
    fix(deps): update dependency zod to v4.1.13

---
+++ assets/chunks/default.CDfu7JfM.js
@@ -0,0 +1,1 @@
+import{r as s,M as a,z as c,Y as p,w as r}from"./framework.C5TORafN.js";import"./theme.x-ExCrrP.js";const f=s({__name:"default",setup(l){const e=a([10,20,50][0]),t=o=>{e.value=o};return(o,i)=>{const n=p("oc-page-size");return r(),c(n,{label:"Items per page",options:[10,20,50],selected:e.value,onChange:t},null,8,["selected"])}}});export{f as default};
