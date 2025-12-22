a007cba871b92e86199d5c5338c20e5674cee453
openclouders <devops@opencloud.eu>
2025-12-22 00:24:23 +0000

    [tx] updated from transifex

--- assets/chunks/default.CnwM3RQ3.js
+++
@@ -1,1 +0,0 @@
-import{r as c,M as n,s,a0 as r,I as l,y as m,W as p,w as i,Y as _}from"./framework.DeuDzT_g.js";import"./theme.DQ2aU8rS.js";const k=c({__name:"default",setup(u){const e=n(),t=o=>{e.value=o};return(o,d)=>{const a=_("oc-emoji-picker");return i(),s(p,null,[r(a,{onEmojiSelect:t}),l("p",null,"Selected emoji: "+m(e.value),1)],64)}}});export{k as default};
