f5e415587142418519590c1b607487557a187cb2
Jannik Stehle <j.stehle@opencloud.eu>
2026-01-16 14:21:27 +0100

    refactor: SpaceInfo component to script setup

--- packages/web-pkg/src/components/SideBar/Spaces/SpaceInfo.vue
+++ packages/web-pkg/src/components/SideBar/Spaces/SpaceInfo.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="grid items-center p-2">
     <div class="flex items-center text-sm">
-      <div class="mr-2">
-        <oc-icon
-          name="layout-grid"
-          :size="resource.description ? 'large' : 'medium'"
-          class="block"
-        />
-      </div>
+      <oc-icon
+        name="layout-grid"
+        :size="resource.description ? 'large' : 'medium'"
+        class="block mr-2"
+      />
       <div>
         <h3
           data-testid="space-info-name"
@@ -20,16 +18,9 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, inject } from 'vue'
+<script setup lang="ts">
+import { inject } from 'vue'
 import { SpaceResource } from '@opencloud-eu/web-client'
 
-export default defineComponent({
-  name: 'SpaceInfo',
-  setup() {
-    return {
-      resource: inject<SpaceResource>('resource')
-    }
-  }
-})
+const resource = inject<SpaceResource>('resource')
 </script>
