bc8e2caeee475a38efefc456b6b3ff62aa745189
Alexander Ackermann <abackermann91@gmail.com>
2026-02-12 12:58:25 +0100

    fix typo

--- packages/web-runtime/src/container/sse/common.ts
+++ packages/web-runtime/src/container/sse/common.ts
@@ -2,7 +2,7 @@ import { SSEEventOptions } from './types'
 
 export const onSSEBackchannelLogoutEvent = ({ router, authStore, sseData }: SSEEventOptions) => {
   if (!sseData.sessionid) {
-    // Log out all client when no session id is provided according to OIDC spec
+    // Log out all clients when no session id is provided according to OIDC spec
     return router.push({ name: 'logout' })
   }
 
