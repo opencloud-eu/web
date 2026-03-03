367f516de5291a187d612dcd8e0a37e32a323983
Alexander Ackermann <abackermann91@gmail.com>
2026-03-03 10:00:34 +0100

    only call sse logout when the event's sessionId matches the active one

--- packages/web-runtime/src/container/sse/common.ts
+++ packages/web-runtime/src/container/sse/common.ts
@@ -1,11 +1,6 @@
 import { SSEEventOptions } from './types'
 
 export const onSSEBackchannelLogoutEvent = ({ router, authStore, sseData }: SSEEventOptions) => {
-  if (!sseData.sessionid) {
-    // Log out all clients when no session id is provided according to OIDC spec
-    return router.push({ name: 'logout' })
-  }
-
   if (authStore.sessionId === sseData.sessionid) {
     return router.push({ name: 'logout' })
   }
