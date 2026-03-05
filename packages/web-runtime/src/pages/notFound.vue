6470fd31efe6d80599d8a753c213e537b280443a
Alexander Ackermann <abackermann91@gmail.com>
2026-03-05 16:06:46 +0100

    translate 404

--- packages/web-runtime/src/pages/notFound.vue
+++ packages/web-runtime/src/pages/notFound.vue
@@ -1,7 +1,7 @@
 <template>
   <no-content-message img-src="/images/empty-states/404.svg" class="h-full">
     <template #message>
-      <span v-text="404" />
+      <span v-text="$gettext('404')" />
     </template>
     <template #callToAction>
       <span v-text="$gettext('The page you are looking for does not exist')" />
