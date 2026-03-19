23239c48f3f36b3818d1d825b2886b6ea68ee043
Pascal Wengerter <pascal@wengerter.info>
2026-03-19 08:59:19 +0100

    Replace @vueuse/head with unhead's vue plugin

--- packages/web-runtime/src/composables/head/useHead.ts
+++ packages/web-runtime/src/composables/head/useHead.ts
@@ -1,5 +1,5 @@
 import { computed } from 'vue'
-import { useHead as _useHead } from '@vueuse/head'
+import { useHead as _useHead } from '@unhead/vue'
 import { getBackendVersion, getWebVersion, useCapabilityStore } from '@opencloud-eu/web-pkg'
 
 export const useHead = () => {
