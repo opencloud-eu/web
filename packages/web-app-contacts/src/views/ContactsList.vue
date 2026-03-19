8b83eafe24572391f1366f65e6bc9dd33676ed01
Alexander Ackermann <abackermann91@gmail.com>
2026-03-19 13:23:44 +0100

    reintroduce sorting as backend is not capable yet

--- packages/web-app-contacts/src/views/ContactsList.vue
+++ packages/web-app-contacts/src/views/ContactsList.vue
@@ -5,7 +5,7 @@
     </div>
     <template v-else>
       <no-content-message
-        v-if="!contacts.length"
+        v-if="!sortedContacts.length"
         id="contacts-empty"
         img-src="/images/empty-states/empty-contacts.svg"
       >
@@ -15,7 +15,7 @@
       </no-content-message>
       <oc-list v-else>
         <li
-          v-for="contact in contacts"
+          v-for="contact in sortedContacts"
           :key="contact.id"
           class="border-b border-role-surface-container-highest last:border-b-0"
         >
@@ -33,9 +33,36 @@ import { useLoadContacts } from '../composables/useLoadContacts'
 import { useContactsStore } from '../composables/piniaStores/contacts'
 import { storeToRefs } from 'pinia'
 import ContactsListItem from '../components/ContactsListItem.vue'
+import { getContactNameParts } from '../helpers/contactName'
+import { computed } from 'vue'
 
 const { isLoading } = useLoadContacts()
 const { $gettext } = useGettext()
 const contactsStore = useContactsStore()
 const { contacts } = storeToRefs(contactsStore)
+
+const sortedContacts = computed(() => {
+  return [...contacts.value].sort((a, b) => {
+    const aNameParts = getContactNameParts(a)
+    const bNameParts = getContactNameParts(b)
+
+    const byGiven = aNameParts.givenName.localeCompare(bNameParts.givenName, undefined, {
+      sensitivity: 'base'
+    })
+
+    if (byGiven !== 0) {
+      return byGiven
+    }
+
+    const bySurname = aNameParts.surname.localeCompare(bNameParts.surname, undefined, {
+      sensitivity: 'base'
+    })
+
+    if (bySurname !== 0) {
+      return bySurname
+    }
+
+    return a.id.localeCompare(b.id, undefined, { sensitivity: 'base' })
+  })
+})
 </script>
