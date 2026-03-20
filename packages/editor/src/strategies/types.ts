eb239d99e957fc7ddc5e6ee5422e6bad2b7ecd82
Benedikt Kulmann <b.kulmann@opencloud.eu>
2026-03-20 16:07:09 +0100

    feat(editor): add plain text strategy

---
+++ packages/editor/src/strategies/types.ts
@@ -0,0 +1,10 @@
+import type { Extension } from '@tiptap/core'
+import type { Editor } from '@tiptap/vue-3'
+import type { ToolbarGroup } from '../toolbar/types'
+
+export interface ContentTypeStrategy {
+  extensions(): Extension[]
+  toolbarItems(): ToolbarGroup[]
+  serialize(editor: Editor): string
+  deserialize(content: string): Record<string, unknown> | string
+}
