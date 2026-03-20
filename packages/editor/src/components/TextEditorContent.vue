f150974801686da7d695dafc3c9a37d09e10d127
Benedikt Kulmann <b.kulmann@opencloud.eu>
2026-03-20 16:15:43 +0100

    feat(editor): add TextEditorProvider, TextEditorContent, TextEditorToolbar components

---
+++ packages/editor/src/components/TextEditorContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <EditorContent v-if="textEditor.editor.value" :editor="textEditor.editor.value" />
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue'
+import { EditorContent } from '@tiptap/vue-3'
+import type { TextEditorInstance } from '../types'
+
+const textEditor = inject<TextEditorInstance>('textEditor')!
+</script>
