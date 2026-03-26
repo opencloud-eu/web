0e305da5b5a8928760127b9e3f5edffd6283a35a
smoothscholar <axb181@gmail.com>
2026-03-26 15:31:29 -0400

    Update config.ts for prettier

--- packages/web-pkg/src/composables/piniaStores/config/config.ts
+++ packages/web-pkg/src/composables/piniaStores/config/config.ts
@@ -90,7 +90,8 @@ export const useConfigStore = defineStore('config', () => {
       unref(options).routing.idBased = !unref(options).cernFeatures
       unref(options).routing.fullShareOwnerPaths = unref(options).cernFeatures
       // openFilesInNewTab can be set independently or derived from cernFeatures
-      unref(options).openFilesInNewTab = unref(options).openFilesInNewTab || unref(options).cernFeatures
+      unref(options).openFilesInNewTab =
+          unref(options).openFilesInNewTab || unref(options).cernFeatures
     }
 
     if (data.external_apps) {
