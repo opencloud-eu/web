0fa6f11fd571bcb99a584c90c054e5db7e2ca416
Alexander Wolff <contact@alexwolff.biz>
2026-04-02 17:25:35 +1000

    Cancel debouncedSearch onKeyUpEnter

--- packages/web-app-search/src/portals/SearchBar.vue
+++ packages/web-app-search/src/portals/SearchBar.vue
@@ -276,6 +276,10 @@ export default defineComponent({
         return
       }
 
+      if (isLocationCommonActive(router, 'files-common-search')) {
+        debouncedSearch.cancel()
+      }
+
       if (unref(optionsDrop)) {
         unref(optionsDrop)?.hide()
       }
