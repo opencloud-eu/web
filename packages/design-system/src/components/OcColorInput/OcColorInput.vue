e503a2ff437939bf4a6792cd856e02a1c8449cfd
Alexander Ackermann <abackermann91@gmail.com>
2026-04-20 22:35:31 +0200

    feat: redesign color picker

--- packages/design-system/src/components/OcColorInput/OcColorInput.vue
+++ packages/design-system/src/components/OcColorInput/OcColorInput.vue
@@ -1,72 +1,79 @@
 <template>
-  <div :class="$attrs.class">
-    <slot name="label">
-      <label class="inline-block mb-0.5" :for="id">
-        {{ label }}
-        <span v-if="requiredMark" class="text-role-error" aria-hidden="true">*</span>
-      </label>
-    </slot>
-    <div class="oc-color-input-wrapper relative max-m-5">
-      <input
-        :id="id"
-        v-bind="additionalAttributes"
-        type="color"
-        :aria-invalid="ariaInvalid"
-        class="oc-color-input oc-input rounded-sm py-0.5 focus:border focus:border-role-outline focus:outline-2 focus:outline-role-outline"
-        :class="{
-          'oc-color-input-danger text-role-error focus:text-role-error border-role-error':
-            !!errorMessage,
-          'pr-6': showClearButton
-        }"
-        :value="modelValue"
-        :disabled="disabled"
-        @change="onChange(($event.target as HTMLInputElement).value)"
-        @input="onInput(($event.target as HTMLInputElement).value)"
-      />
-      <oc-button
-        v-if="showClearButton"
-        class="mr-1 absolute top-[50%] transform-[translateY(-50%)] right-0 oc-color-input-btn-clear"
-        appearance="raw"
-        no-hover
-        @click="onClear"
-      >
-        <oc-icon name="close" size="small" />
-      </oc-button>
-    </div>
-    <div
-      v-if="showMessageLine"
-      class="oc-color-input-message flex items-center text-sm mt-1 min-h-4.5"
-      :class="{
-        'oc-color-input-description text-role-on-surface-variant relative': !!descriptionMessage,
-        'oc-color-input-danger text-role-error focus:text-role-error border-role-error':
-          !!errorMessage
-      }"
-    >
-      <oc-icon
-        v-if="!!errorMessage"
-        name="error-warning"
-        size="small"
-        fill-type="line"
-        aria-hidden="true"
-        class="mr-1"
-      />
+  <div class="min-w-45 w-fit bg-role-surface-container rounded-xl" :class="$attrs.class">
+    <slot name="preview" :color="modelValue" />
+    <div class="p-4 cursor-pointer" @click="openColorPicker">
+      <slot name="label">
+        <label class="inline-block mb-2 font-semibold cursor-pointer" :for="id">
+          {{ label }}
+          <span v-if="requiredMark" class="text-role-error" aria-hidden="true">*</span>
+        </label>
+      </slot>
+      <div class="flex items-center justify-between">
+        <span class="text-sm text-role-on-surface-variant uppercase" v-text="modelValue" />
+        <div class="oc-color-input-wrapper flex">
+          <input
+            :id="id"
+            ref="colorInputRef"
+            v-bind="additionalAttributes"
+            type="color"
+            :aria-invalid="ariaInvalid"
+            class="oc-color-input rounded-full w-4 h-4 overflow-hidden pointer-events-none border-2 border-role-outline-variant focus:border focus:border-role-outline focus:outline-2 focus:outline-role-outline"
+            :class="{
+              'oc-color-input-danger text-role-error focus:text-role-error border-role-error':
+                !!errorMessage
+            }"
+            :value="modelValue"
+            :disabled="disabled"
+            @change="onChange(($event.target as HTMLInputElement).value)"
+            @input="onInput(($event.target as HTMLInputElement).value)"
+          />
+          <oc-button
+            v-if="showClearButton"
+            class="oc-color-input-btn-clear ml-1 pointer-events-auto"
+            appearance="raw"
+            no-hover
+            @click.stop="onClear"
+          >
+            <oc-icon name="close" size="small" />
+          </oc-button>
+        </div>
+        <div
+          v-if="showMessageLine"
+          class="oc-color-input-message flex items-center text-sm mt-1 min-h-4.5"
+          :class="{
+            'oc-color-input-description text-role-on-surface-variant relative':
+              !!descriptionMessage,
+            'oc-color-input-danger text-role-error focus:text-role-error border-role-error':
+              !!errorMessage
+          }"
+        >
+          <oc-icon
+            v-if="!!errorMessage"
+            name="error-warning"
+            size="small"
+            fill-type="line"
+            aria-hidden="true"
+            class="mr-1"
+          />
 
-      <span
-        :id="messageId"
-        :class="{
-          'oc-color-input-description text-role-on-surface-variant flex items-center':
-            !!descriptionMessage,
-          'oc-color-input-danger text-role-error focus:text-role-error border-role-error':
-            !!errorMessage
-        }"
-        v-text="messageText"
-      />
+          <span
+            :id="messageId"
+            :class="{
+              'oc-color-input-description text-role-on-surface-variant flex items-center':
+                !!descriptionMessage,
+              'oc-color-input-danger text-role-error focus:text-role-error border-role-error':
+                !!errorMessage
+            }"
+            v-text="messageText"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, HTMLAttributes, useAttrs } from 'vue'
+import { computed, HTMLAttributes, unref, useAttrs, useTemplateRef } from 'vue'
 import { uniqueId } from '../../helpers'
 import OcButton from '../OcButton/OcButton.vue'
 import OcIcon from '../OcIcon/OcIcon.vue'
@@ -131,6 +138,10 @@ export interface Emits {
 
 export interface Slots {
   /**
+   * @docs Can be used to display a preview or showcase.
+   */
+  preview?: (color: string) => unknown
+  /**
    * @docs Can be used to overwrite the default rendering of the label.
    */
   label?: () => unknown
@@ -183,6 +194,15 @@ const showClearButton = computed(() => {
   return !disabled && clearButtonEnabled && !!modelValue
 })
 
+const colorInputRef = useTemplateRef<HTMLInputElement>('colorInputRef')
+
+function openColorPicker() {
+  if (disabled) {
+    return
+  }
+  unref(colorInputRef).click()
+}
+
 function onClear() {
   onInput('')
   onChange(null)
@@ -196,3 +216,19 @@ const onInput = (value: string) => {
   emit('update:modelValue', value)
 }
 </script>
+
+<style scoped>
+@reference '@opencloud-eu/design-system/tailwind';
+
+.oc-color-input::-webkit-color-swatch-wrapper {
+  @apply p-0 rounded-full;
+}
+
+.oc-color-input::-webkit-color-swatch {
+  @apply border-0 rounded-full;
+}
+
+.oc-color-input::-moz-color-swatch {
+  @apply border-0 rounded-full;
+}
+</style>
