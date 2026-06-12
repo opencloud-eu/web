2b3d7e786d369eab479484e482b8c0d4aa9ca17a
Jannik Stehle <j.stehle@opencloud.eu>
2026-06-12 09:21:51 +0200

    fix: version loading on search and favorite pages

--- packages/web-pkg/src/composables/resources/useCanListVersions.ts
+++ packages/web-pkg/src/composables/resources/useCanListVersions.ts
@@ -9,6 +9,11 @@ import {
 export const useCanListVersions = () => {
   const userStore = useUserStore()
 
+  /**
+   * Checks if the versions of a resource can be listed.
+   * Make sure that the graph permissions of the space are loaded before calling this function,
+   * otherwise it might return false even if the user has the required permissions.
+   */
   const canListVersions = ({ space, resource }: { space: SpaceResource; resource: Resource }) => {
     const spaceAllowsListingVersions =
       (isPersonalSpaceResource(space) && space.isOwner(userStore.user)) ||
