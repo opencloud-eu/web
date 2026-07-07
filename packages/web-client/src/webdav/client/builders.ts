e0b06a083d94885f4dcd8fac2c547db1ccc9a918
Jannik Stehle <j.stehle@opencloud.eu>
2026-07-07 09:51:05 +0200

    style: run formatter

--- packages/web-client/src/webdav/client/builders.ts
+++ packages/web-client/src/webdav/client/builders.ts
@@ -41,7 +41,10 @@ export const buildPropFindBody = (
     bodyType = 'oc:filter-files'
   }
 
-  const object = properties.reduce<Record<string, unknown>>((obj, item) => Object.assign(obj, { [item]: null }), {})
+  const object = properties.reduce<Record<string, unknown>>(
+    (obj, item) => Object.assign(obj, { [item]: null }),
+    {}
+  )
   // Include extra props in the request so they appear in PROPFIND
   for (const ep of extraProps) {
     if (!(ep in object)) {
