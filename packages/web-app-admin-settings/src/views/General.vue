9150fdc6a55bf7c090f82b83f9b3d87772b3eb9a
Alexander Ackermann <abackermann91@gmail.com>
2026-07-16 13:17:38 +0200

    adjust details panel

--- packages/web-app-admin-settings/src/views/General.vue
+++ packages/web-app-admin-settings/src/views/General.vue
@@ -7,7 +7,6 @@
         to: { name: 'admin-settings-general' }
       }
     ]"
-    :side-bar-available-panels="sideBarAvailablePanels"
   >
     <template #mainContent>
       <div class="px-4">
@@ -20,19 +19,4 @@
 <script setup lang="ts">
 import AppTemplate from '../components/AppTemplate.vue'
 import InfoSection from '../components/General/InfoSection.vue'
-import DetailsPanel from '../components/General/SideBar/DetailsPanel.vue'
-import { useGettext } from 'vue3-gettext'
-
-const { $gettext } = useGettext()
-
-const sideBarAvailablePanels = [
-  {
-    name: 'DetailsPanel',
-    icon: 'settings-4',
-    title: () => $gettext('Details'),
-    component: DetailsPanel,
-    isRoot: () => true,
-    isVisible: () => true
-  }
-]
 </script>
