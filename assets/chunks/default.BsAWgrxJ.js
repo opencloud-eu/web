c3a7f5eb3ad8e9831c216d9e416f6b2163aace9c
AlexAndBear <abackermann91@gmail.com>
2026-07-22 04:56:08 +0000

    fix(tip tap): emoji picker causes visual glitches with limited screen space (#2936)

---
+++ assets/chunks/default.BsAWgrxJ.js
@@ -0,0 +1,1 @@
+import"./theme.Ds4i6a5f.js";import{v as c,w as n,a2 as s,L as r,B as l,Z as m,Q as p,z as i,a0 as _}from"./framework.sEtmYSQY.js";const k=c({__name:"default",setup(u){const e=p(),t=o=>{e.value=o};return(o,d)=>{const a=_("oc-emoji-picker");return i(),n(m,null,[s(a,{onEmojiSelect:t}),r("p",null,"Selected emoji: "+l(e.value),1)],64)}}});export{k as default};
