d0a22674fa8b34825637e8cf5cafb3823e778e64
Jannik Stehle <j.stehle@opencloud.eu>
2026-07-28 16:53:31 +0200

    feat(admin-settings): paginate space members in right sidebar

--- packages/web-app-admin-settings/tests/unit/components/Spaces/SideBar/MembersRoleSection.spec.ts
+++ packages/web-app-admin-settings/tests/unit/components/Spaces/SideBar/MembersRoleSection.spec.ts
@@ -12,7 +12,11 @@ describe('MembersRoleSection', () => {
       })
     ]
     const { wrapper } = getWrapper({ permissions })
-    expect(wrapper.html()).toMatchSnapshot()
+    const members = wrapper.findAll('[data-testid="space-members-list"]')
+
+    expect(members.map((member) => member.text())).toEqual(['einstein', 'physic-lovers'])
+    expect(members[0].find('user-avatar-stub').exists()).toBeTruthy()
+    expect(members[1].find('oc-avatar-item-stub').exists()).toBeTruthy()
   })
 })
 
