595d216281cc20ab3ff9c599c58d311e059d04f3
dschmidt <dev@dominik-schmidt.de>
2026-07-30 10:06:08 +0000

    Merge pull request #2967 from opencloud-eu/feat/announcement-banner
    feat: add announcement banner

---
+++ assets/chunks/default.DWOPQzY7.js
@@ -0,0 +1,1 @@
+import"./theme.BoDnmgkB.js";import{v as t,C as r,Q as p,a0 as s,z as c}from"./framework.yxG6kJ00.js";const m=t({__name:"default",setup(i){const e=p(1);return(u,n)=>{const a=s("oc-pagination-inline");return c(),r(a,{"current-page":e.value,"onUpdate:currentPage":n[0]||(n[0]=o=>e.value=o),pages:3},null,8,["current-page"])}}});export{m as default};
