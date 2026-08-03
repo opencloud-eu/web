ffe859793bef9d8a0217d10b4d63062724f60c1b
JammingBen <50302941+JammingBen@users.noreply.github.com>
2026-08-03 08:34:24 +0000

    Merge pull request #3008 from opencloud-eu/chore/prepare-7.3.0-release
    chore: prepare 7.3.0 release

---
+++ assets/chunks/default.PB7sp1c8.js
@@ -0,0 +1,1 @@
+import"./theme.D2LUHykG.js";import{v as t,C as r,Q as p,a0 as s,z as c}from"./framework.DRSAAGEP.js";const m=t({__name:"default",setup(i){const e=p(1);return(u,n)=>{const a=s("oc-pagination-inline");return c(),r(a,{"current-page":e.value,"onUpdate:currentPage":n[0]||(n[0]=o=>e.value=o),pages:3},null,8,["current-page"])}}});export{m as default};
