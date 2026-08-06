f2781001051d2b1a35242159b4185cea738d143f
AlexAndBear <abackermann91@gmail.com>
2026-08-06 06:48:45 +0000

    chore: fix several linter warnings (#3042)

--- assets/chunks/default.DU1DeBRO.js
+++
@@ -1,1 +0,0 @@
-import"./theme.Ce7mzm79.js";import{v as t,C as r,Q as p,a0 as s,z as c}from"./framework.CGNOlobm.js";const m=t({__name:"default",setup(i){const e=p(1);return(u,n)=>{const a=s("oc-pagination-inline");return c(),r(a,{"current-page":e.value,"onUpdate:currentPage":n[0]||(n[0]=o=>e.value=o),pages:3},null,8,["current-page"])}}});export{m as default};
